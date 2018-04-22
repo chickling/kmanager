2cd26e4d76965da1d02769d80317950855095195
Hulva Luva.H <lvsweet217@163.com>
2018-04-22 20:49:56 +0800

    add kafka consumer demo; OffsetFetchTest

--- src/test/java/com/chickling/kmanager/test/OffsetFetchTest.java
+++ src/test/java/com/chickling/kmanager/test/OffsetFetchTest.java
@@ -3,7 +3,9 @@
  */
 package com.chickling.kmanager.test;
 
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 import java.nio.ByteBuffer;
 
 import org.apache.kafka.common.serialization.Serdes;
@@ -11,6 +13,7 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.json.JSONObject;
 
 import kafka.common.OffsetAndMetadata;
 import kafka.coordinator.BaseKey;
@@ -24,46 +27,63 @@ import kafka.coordinator.OffsetKey;
  *
  */
 public class OffsetFetchTest {
-  private final static String OFFSET_TOPIC = "__consumer_offsets";
-  private final static String BOOTSTRAP_SERVERS = "10.16.238.101:8092,10.16.238.102:8092";
-  private final static String OUTPUT_TOPIC = "__consumer_offsets_output";
+	private final static String OFFSET_TOPIC = "__consumer_offsets";
+	// private final static String BOOTSTRAP_SERVERS =
+	// "10.16.238.101:8092,10.16.238.102:8092";
+	private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+	private final static String OUTPUT_TOPIC = "__consumer_offsets_output";
+	private final static Set<String> EXCEPT_TOPIC = new HashSet<String>();
 
-  public static void main(String[] args) {
-    Properties props = new Properties();
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "consumer-offsets-consumer-app");
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-    props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
-    props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
-    props.put("exclude.internal.topics", "false"); // necessary to consume __consumer_offsets
+	static {
+		EXCEPT_TOPIC.add(OFFSET_TOPIC);
+		EXCEPT_TOPIC.add(OUTPUT_TOPIC);
+	}
 
-    KStreamBuilder builder = new KStreamBuilder();
-    builder.stream(Serdes.ByteArray(), Serdes.ByteArray(), OFFSET_TOPIC)
-        .map((k, v) -> new KeyValue<BaseKey, byte[]>(GroupMetadataManager.readMessageKey(ByteBuffer.wrap(k)), v))
-        .filter((k, v) -> {
-          if (k instanceof OffsetKey) {
-            OffsetKey offsetKey = (OffsetKey) k;
-            return offsetKey.key().topicPartition().topic() != OUTPUT_TOPIC;
-          }
-          // if (k instanceof GroupMetadataKey) {
-          // GroupMetadataKey key = (GroupMetadataKey) k;
-          // System.out.println(key.toString());
-          // }
-          return false;
-        })
-        .map((k, v) -> {
-          System.out.println(k.toString());
-          // v 有可能是 null
-          if (v == null) {
-            return new KeyValue<OffsetKey, OffsetAndMetadata>((OffsetKey) k, null);
-          }
-          return new KeyValue<OffsetKey, OffsetAndMetadata>((OffsetKey) k, GroupMetadataManager.readOffsetMessageValue(ByteBuffer.wrap(v)));
-        })
-        .foreach((key, offsetAndMetadata) -> {
-          System.out.println(String.format("key: {}, offsetAndMetadata: {}", key, offsetAndMetadata == null ? null : offsetAndMetadata.toString()));
-        });
-    
-    KafkaStreams streams = new KafkaStreams(builder, props);
-    streams.start();
-  }
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "consumer-offsets-consumer-app");
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		props.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
+		props.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
+		props.put("exclude.internal.topics", "false"); // necessary to consume __consumer_offsets
+
+		KStreamBuilder builder = new KStreamBuilder();
+		builder.stream(Serdes.ByteArray(), Serdes.ByteArray(), OFFSET_TOPIC)
+				// @formatter:off
+				.map((k, v) -> new KeyValue<BaseKey, byte[]>(GroupMetadataManager.readMessageKey(ByteBuffer.wrap(k)),
+						v))
+				.filter((k, v) -> {
+					if (k instanceof OffsetKey) {
+						OffsetKey offsetKey = (OffsetKey) k;
+						System.out.println(offsetKey.key().topicPartition().topic());
+						return !EXCEPT_TOPIC.contains(offsetKey.key().topicPartition().topic());
+					}
+					// if (k instanceof GroupMetadataKey) {
+					// GroupMetadataKey key = (GroupMetadataKey) k;
+					// System.out.println(key.toString());
+					// }
+					return false;
+				})
+				.map((k, v) -> new KeyValue<OffsetKey, OffsetAndMetadata>((OffsetKey) k,
+						/* v 有可能是 null */ v == null ? null
+								: GroupMetadataManager.readOffsetMessageValue(ByteBuffer.wrap(v))))
+				// .to(OUTPUT_TOPIC)
+				.foreach((key, offsetAndMetadata) -> {
+					JSONObject json = new JSONObject();
+					json.put("group", key.key().group());
+					json.put("topic", key.key().topicPartition().topic());
+					json.put("partition", key.key().topicPartition().partition());
+					json.put("version", key.version());
+					json.put("offset", offsetAndMetadata.offset());
+					json.put("commitTimestamp", offsetAndMetadata.commitTimestamp());
+					json.put("expireTimestamp", offsetAndMetadata.expireTimestamp());
+					json.put("metadata", offsetAndMetadata.metadata());
+					System.out.println(json.toString());
+				});
+		// @formatter:on
+
+		KafkaStreams streams = new KafkaStreams(builder, props);
+		streams.start();
+	}
 
 }
